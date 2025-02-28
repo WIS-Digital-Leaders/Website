0c2086e47fbb0a5e40e7ac4f84779d3f6b3262b6
boblamceo <boblam.ceo@gmail.com>
2025-02-28 22:24:42 +0800

    css

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ function navigate(page) {
         window.location.href = page;
     }, 1000);
 }
+
+let cursor = document.getElementById("cursor");
+function isTouchDevice() {
+    try {
+        document.createEvent("TouchEvent");
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+const move = (e) => {
+    try {
+        var x = !isTouchDevice() ? e.pageX : e.touches[0].pageX;
+        var y = !isTouchDevice() ? e.pageY : e.touches[0].pageY;
+    } catch (e) {}
+    cursor.style.left = x - 80 + "px";
+    cursor.style.top = y - 20 + "px";
+};
+document.addEventListener("mousemove", (e) => {
+    move(e);
+});
+document.addEventListener("touchmove", (e) => {
+    move(e);
+});
