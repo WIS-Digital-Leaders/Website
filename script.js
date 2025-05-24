c2e3b9490c7de7bd174d27df6acc4fbdafad328d
Gerald987 <geraldlowe9@gmail.com>
2025-05-24 11:45:25 +0800

    Remove unused event listener for DOMContentLoaded and related tree toggler functionality

--- script.js
+++ script.js
@@ -61,16 +61,3 @@ document.addEventListener("mousemove", (e) => {
 document.addEventListener("touchmove", (e) => {
     move(e);
 });
-
-
-
-//tree stuff
-document.addEventListener('DOMContentLoaded', function() {
-    var toggler = document.getElementsByClassName("caret");
-    for (var i = 0; i < toggler.length; i++) {
-        toggler[i].addEventListener("click", function() {
-            this.parentElement.querySelector(".nested").classList.toggle("active");
-            this.classList.toggle("caret-down");
-        });
-    }
-});
